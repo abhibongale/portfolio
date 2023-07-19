990800df837b382adad77e40931fa71ccca6d58c
abhibongale <abhishekbongale@outlook.com>
2023-07-19 01:00:18 +0100

    [P-01, P-02] - Setup Application and Display home page
    * As a User, I want to see the home page with developer details
    * Home page should contain the developer name, description
    * Home page should contain the navigation bar containing the blog link and contact link
    * as a developer, I will be using nextJS to develop the website
    * sanity for website content management.

---
+++ src/sanity/sanity-utils.ts
@@ -0,0 +1,23 @@
+import { createClient, groq } from "next-sanity";
+import {Project} from '@/types/project'
+
+export async function getProjects(): Promise<Project[]> {
+    const client = createClient({
+        projectId: "p20np4gp",
+        dataset: "production",
+        apiVersion: '2023-07-15'
+    });
+
+    return client.fetch(
+        groq`*[_type == "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+          }`
+    );
+    
+}
