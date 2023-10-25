7f9623471cbe0dfbe4d090e871f0b806223a46cc
Abhishek Bongale <abhishekbongale@outlook.com>
2023-10-25 21:44:34 +0100

    Update next.config.js
    update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/portfolio/',
-  assetPrefix: "./",
+  assetPrefix: "/",
 };
 
 module.exports = withContentlayer(nextConfig);
