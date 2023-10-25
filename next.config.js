d3b8ee46db802981859b654d87f85a60b75a14ce
Abhishek Bongale <abhishekbongale@outlook.com>
2023-10-25 20:36:24 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/portfolio',
   swcMinify: true,
 };
 
