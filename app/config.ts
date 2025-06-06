4cf44df9ef70187bf5337f0c2c1460921d834cd4
Abhishek Bongale <abhishekbongale@outlook.com>
2025-06-06 21:49:12 +0100

    added export to fix the github actions

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
   title: "Abhishek Bongale",
-  name: "Sirius",
+  name: "abhibongale",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/abhibongale",
+  github: "https://github.com/abhibongale",
+  //instagram: "https://www.instagram.com/abhibongale",
+  //linkedin: "https://www.linkedin.com/",
+  email: "mailto:abhishekpbongale@gmail.com",
 };
